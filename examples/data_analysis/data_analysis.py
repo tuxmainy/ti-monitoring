a0f83b6c81f20672a27212fae43dca0200072c4d
Lukas Schmidt-Russnak <l.schmidt-russnak@gmx.de>
2025-08-10 22:26:25 +0200

    added example for data analysis

---
+++ examples/data_analysis/data_analysis.py
@@ -0,0 +1,53 @@
+from mylibrary import *
+from myconfig import *
+
+import matplotlib.pyplot as plt
+
+# get basic and availability data for all cis
+all_data = []
+for index, ci in get_data_of_all_cis(file_name).iterrows():
+    availability_data = get_availability_data_of_ci(file_name, ci['ci'])
+    first_timestamp = availability_data['times'].iloc[0]
+    last_timestamp = availability_data['times'].iloc[-1]
+    expected_number_of_data_points = int(round(pd.Timedelta(last_timestamp - first_timestamp).total_seconds())/300)
+    number_of_dropped_data_points = expected_number_of_data_points - len(availability_data['values'])
+    entry = {
+        "ci_name" : ci['ci'] + ', ' + ci['name'] + '\n' + ci['organization'],
+        "available" : len(availability_data[availability_data['values']==1]),
+        "unavailable" : len(availability_data[availability_data['values']==0]),
+        "unknown" : number_of_dropped_data_points,
+        "first_timestamp" : first_timestamp,
+        "last_timestamp" : last_timestamp,
+        "expected_number_of_data_points" : expected_number_of_data_points
+    }
+    all_data.append(entry)
+all_data = pd.DataFrame(all_data)
+
+# plot cis that were unavailable
+plot_data = all_data[all_data['unavailable']>0].sort_values(by = 'unavailable')
+data_start = plot_data['first_timestamp'].min().strftime('%d.%m.%Y %H:%M:%S Uhr')
+data_end = plot_data['last_timestamp'].max().strftime('%d.%m.%Y %H:%M:%S Uhr')
+names = plot_data['ci_name']
+data = plot_data['unavailable'].values*5
+fig, ax = plt.subplots(figsize = (10,10))
+ax.tick_params(labelsize = 8)
+ax.tick_params(axis='x', labelsize = 12)
+barh = ax.barh(names, data, 0.5)
+ax.bar_label(barh)
+ax.text(
+    data.max()/4,
+    len(data)/4,
+    """Die Berechung der Ausfallzeiten erfolgte durch Multiplikation
+der Anzahl der Datenpunkte 'nicht verfügbar' mit 5 Minuten,
+der Dauer eines Abfrageintervalls. Die Daten wurden von der
+öffentlichen TI-Lage-Schnittstelle der gematik GmbH abgerufen.
+Bei der Interpretation sind demensprechend die Hinweise in der
+Dokumentation der Schnittstelle zu berücksichtigen. Weitere
+Informationen unter https://github.com/gematik/api-tilage"""
+)
+ax.set_title("Störungen zentraler TI-Komponenten\nim Zeitraum " + data_start + ' bis ' + data_end)
+ax.set_xlabel('Störungsdauer in Minuten')
+plt.tight_layout()
+#plt.show()
+export_file_path = 'examples/data_analysis/plot_unavailable_cis.png'
+plt.savefig(export_file_path, dpi = 600)
